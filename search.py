e3780b47141eba6e8a815a68e4a3af3009a42d84
Hui Su <hsu031@ucr.edu>
2020-10-30 22:04:23 -0700

    dfs

--- search.py
+++ search.py
@@ -87,6 +87,26 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
+    from util import Stack
+    fringe = Stack()                # Fringe to manage which states to expand
+    fringe.push(problem.getStartState())
+    visited = []                    # List to check whether state has already been visited
+    path=[]                         # Final direction list
+    pathToCurrent=Stack()           # Stack to maintaing path from start to a state
+    currState = fringe.pop()
+    while not problem.isGoalState(currState):
+        if currState not in visited:
+            visited.append(currState)
+            successors = problem.getSuccessors(currState)
+            for child,direction,cost in successors:
+                fringe.push(child)
+                tempPath = path + [direction]
+                pathToCurrent.push(tempPath)
+        currState = fringe.pop()
+        path = pathToCurrent.pop()
+    return path
+    
+    
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
